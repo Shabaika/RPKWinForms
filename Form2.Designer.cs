c7965be1379dabdebb73342eae3103138eeebecb
Shabaika <gorod.trav@mail.ru>
2022-04-18 22:46:53 +0300

    reload

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -338,6 +338,7 @@
             this.button1.TabIndex = 32;
             this.button1.Text = "Вычисление";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
